99a1d3b4e1139d55c0b0d53c7c8321fcb3075dc4
Rudraditya Thakur <rudraditya.thakur21@gmail.com>
2025-12-12 23:39:56 +0530

    added: smooth scroll, added: about, projects, and work section, updated component: curved loop

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { BBH_Sans_Bogle, BBH_Sans_Bartle } from "next/font/google"
 import CurvedLoop from "@/components/curved-loop"
+import Link from "next/link"
 
 const bbhSansBoggle = BBH_Sans_Bogle({
   weight: '400',
@@ -18,19 +19,28 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center">
           <p className={`${bbhSansBartle.className} text-center md:text-7xl lg:text-9xl `}> # ENGINEER </p>
           <div className="flex w-3xl lg:w-6xl text-md lg:text-lg items-center justify-between tracking-widest">
-            <p className="">{`[ABOUT]`}</p>
-            <p className="">{`[PROJECTS]`}</p>
-            <p className="">{`[WORK]`}</p>
+            <Link href="#about" className="">{`[ABOUT]`}</Link>
+            <Link href="#projects" className="">{`[PROJECTS]`}</Link>
+            <Link href="#work" className="">{`[WORK]`}</Link>
           </div>
         </div>
         <CurvedLoop
           marqueeText="Built Different. Engineered Better."
           speed={3}
-          curveAmount={200}
+          curveAmount={300}
           direction="right"
           interactive={true}
           className={`${bbhSansBoggle.className} text-md`}
         />
+      </div>
+      <div id="about" className="hidden md:block min-h-svh">
+        <p className={`${bbhSansBartle.className} text-center md:text-7xl lg:text-9xl `}>ABOUT</p>
+      </div>
+      <div id="projects" className="hidden md:block min-h-svh">
+        <p className={`${bbhSansBartle.className} text-center md:text-7xl lg:text-9xl `}>PROJECTS</p>
+      </div>
+      <div id="work" className="hidden md:block min-h-svh">
+        <p className={`${bbhSansBartle.className} text-center md:text-7xl lg:text-9xl `}>WORK</p>
       </div>
     </main>
   )
--- components/curved-loop.tsx
+++ components/curved-loop.tsx
@@ -110,7 +110,7 @@ const CurvedLoop: FC<CurvedLoopProps> = ({
 
   return (
     <div
-      className="min-h-screen flex items-center justify-center w-full"
+      className="min-h-[70svh] flex items-center justify-center w-full"
       style={{ visibility: ready ? 'visible' : 'hidden', cursor: cursorStyle }}
       onPointerDown={onPointerDown}
       onPointerMove={onPointerMove}
