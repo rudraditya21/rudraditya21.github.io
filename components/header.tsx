d1ee5febfd2eb973b48dcb5f6fb7b848e4f9478e
Rudraditya Thakur <rudraditya.thakur21@gmail.com>
2025-12-12 22:41:43 +0530

    added: header, updated: style

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,24 @@
+"use client"
+import { LinkedinLogoIcon, XLogoIcon, GooglePodcastsLogoIcon, GithubLogoIcon } from "@phosphor-icons/react"
+import { BBH_Sans_Bartle } from "next/font/google"
+
+const bbhSansBartle = BBH_Sans_Bartle({
+  weight: '400',
+  subsets: ['latin']
+})
+
 export default function Header() {
   return (
-    <div></div>
+    <div className="py-6 px-8 flex justify-center lg:justify-between items-center">
+      <div className="hidden lg:flex space-x-4">
+        <LinkedinLogoIcon size={22} />
+        <XLogoIcon size={22} />
+      </div>
+      <p className={`${bbhSansBartle.className} text-md md:text-xl lg:text-2xl `}>Rudraditya Thakur</p>
+      <div className="hidden lg:flex space-x-4">
+        <GithubLogoIcon size={22} />
+        <GooglePodcastsLogoIcon size={22} />
+      </div>
+    </div>
   )
 }
